9565f080e5701a5569a47f77b046fbadfd12637e
anaclaralopeslouzada <ana.louzada@unifesp.br>
2026-04-07 16:37:28 -0300

    Update ILogObserver.cs

--- ILogObserver.cs
+++ ILogObserver.cs
@@ -1,7 +1,7 @@
 namespace LogSystem
 {
-    /// Esta interface define o contrato para os Observadores
-    /// Qualquer classe que queira "ouvir" os logs deve implementar esta interface
+    // Esta interface define o contrato para os Observadores
+    // Qualquer classe que queira "ouvir" os logs deve implementar esta interface
     public interface ILogObserver
     {
         // Método chamado pelo Subject sempre que houver uma nova mensagem
